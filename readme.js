908e8f13a76737bf104ae22f912584b604dc4ba6
Natan Stuart <nbs2115@gmail.com>
2021-01-10 17:25:37 -0800

    fixed require markdown

--- readme.js
+++ readme.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require("path");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generate-markdown");
 
 const questions = [
     {
